7e3878ed33e54da51cd78b8b7916dabe52071018
Crownie <crowniedavid@myself.com>
2014-09-09 21:23:50 +0100

    added latex-to-js.js
    The core js file "latex-to-js" and a test folder was added. Readme file
    was updated.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 latex-to-js
 ===========
 
-a JavaScript plugin for converting latex equations to js math syntax
+A JavaScript plugin for converting latex equations to js math syntax.
+
+The output is a js math syntax that is only compatible with [mathjs][]
+
+### Usage
+
+Include latex-to-js.js in your html.Then call the **latex\_to\_js();** function with latex string as the argument. The function will return a js math syntax that can be processed by math js or it will throw a syntax error if there is a problem with the latex string.
+
+The converter currently only supports the following latex math functions; fraction,square,nth root,sin, tan and cos
+
+  [mathjs]: http://mathjs.org
---
+++ latex-to-js.js
@@ -0,0 +1,261 @@
+var latex_to_js = function(input) {
+
+	var init, fraction, square_root, nth_root, nth_power, sin, cos, tan, sinCosTanFramework, convert_others;
+
+	init = function() {
+		var st1 = input;
+		st1 = st1.replace(/\s/g, "");
+		st1 = st1.replace(/\\times/g, "*");
+		st1 = st1.replace(/\\div/g, "/");
+
+		//pi
+		st1 = st1.replace(/([0-9a-zA-Z\.]+)\\pi/g, "$1*3.142");
+		st1 = st1.replace(/\\pi([0-9a-zA-Z\.]+)/g, "3.142*$1");
+		st1 = st1.replace(/([0-9a-zA-Z\.]+)\\pi([0-9a-zA-Z\.]+)/g, "$1*3.142*$2");
+		st1 = st1.replace(/\\pi/g, "3.142");
+
+		st1 = fraction(st1);
+		st1 = square_root(st1);
+		st1 = nth_root(st1);
+		st1 = nth_power(st1);
+		st1 = sin(st1);
+		st1 = tan(st1);
+		st1 = cos(st1);
+
+		//clean up brackets
+		st1 = st1.replace(/\\left\(/g, "(");
+		st1 = st1.replace(/\\right\)/g, ")");
+		return st1;
+	};
+
+	fraction = function(input) {
+		while (input.search(/\\frac\{(((?![\{\}]).)*)\}\{(((?![\{\}]).)*)\}/) >= 0) {
+			console.log(input.match(/\\frac\{(((?![\{\}]).)*)\}\{(((?![\{\}]).)*)\}/));
+			input = input.replace(/\\frac\{(((?![\{\}]).)*)\}\{(((?![\{\}]).)*)\}/g, "($1)/($3)");
+		}
+
+		if (input.search(/\\frac/) >= 0) {
+			input = convert_others("fraction", input);
+		}
+
+		return input;
+	};
+
+	square_root = function(input) {
+		while (input.search(/\\sqrt\{(((?![\{\}]).)*)\}/) >= 0) {
+			console.log(input.match(/\\sqrt\{(((?![\{\}]).)*)\}/));
+			input = input.replace(/\\sqrt\{(((?![\{\}]).)*)\}/g, "sqrt($1)");
+		}
+
+		if (input.search(/\\sqrt\{/) >= 0) {
+			input = convert_others("square root", input);
+		}
+
+		return input;
+	};
+
+	nth_root = function(input) {
+		while (input.search(/\\sqrt\[(((?![\{\}]).)*)\]\{(((?![\{\}]).)*)\}/) >= 0) {
+			console.log(input.match(/\\sqrt\[(((?![\{\}]).)*)\]\{(((?![\{\}]).)*)\}/));
+			input = input.replace(/\\sqrt\[(((?![\{\}]).)*)\]\{(((?![\{\}]).)*)\}/g, "pow($3,1/$1)");
+		}
+		if (input.search(/\\sqrt\[/) >= 0) {
+			input = convert_others("nth root", input);
+		}
+		return input;
+	};
+
+	nth_power = function(input) {
+		//first case: single number with curly bracket power
+		while (input.search(/([0-9a-zA-Z\.]+)\^\{(((?![\{\}]).)*)\}/) >= 0) {
+			console.log(input.match(/([0-9a-zA-Z\.]+)\^\{(((?![\{\}]).)*)\}/));
+			input = input.replace(/([0-9a-zA-Z\.]+)\^\{(((?![\{\}]).)*)\}/g, "pow($1,$2)");
+		}
+		//second case: single number without curly bracket
+		while (input.search(/([0-9a-zA-Z\.]+)\^([0-9a-zA-Z\.]+)/) >= 0) {
+			console.log(input.match(/([0-9a-zA-Z\.]+)\^([0-9a-zA-Z\.]+)/));
+			input = input.replace(/([0-9a-zA-Z\.]+)\^([0-9a-zA-Z\.]+)/g, "pow($1,$2)");
+		}
+
+		//third case: bracket number without curly bracket power
+		while (input.search(/\\left\(([0-9a-zA-Z\.\+\*\-\\]+)\\right\)\^([0-9a-zA-Z\.]+)/) >= 0) {
+			console.log(input.match(/\\left\(([0-9a-zA-Z\.\+\*\-\\]+)\\right\)\^([0-9a-zA-Z\.]+)/));
+			input = input.replace(/\\left\(([0-9a-zA-Z\.\+\*\-\\]+)\\right\)\^([0-9a-zA-Z\.]+)/g, "pow($1,$2)");
+		}
+
+		//forth case: bracket number with curly bracket power
+		while (input.search(/\\left\(([0-9a-zA-Z\.\+\*\-\\]+)\\right\)\^\{(((?![\{\}]).)*)\}/) >= 0) {
+			console.log(input.match(/\\left\(([0-9a-zA-Z\.\+\*\-\\]+)\\right\)\^\{(((?![\{\}]).)*)\}/));
+			input = input.replace(/\\left\(([0-9a-zA-Z\.\+\*\-\\]+)\\right\)\^\{(((?![\{\}]).)*)\}/g, "pow($1,$2)");
+		}
+		
+		//fifth case: bracket number with some brackets and division sign, with curly bracket power
+		while (input.search(/\\left\(([0-9a-zA-Z\.\+\*\-\\\(\)\/]+)\\right\)\^\{(((?![\{\}]).)*)\}/) >= 0) {
+			console.log(input.match(/\\left\(([0-9a-zA-Z\.\+\*\-\\\(\)\/]+)\\right\)\^\{(((?![\{\}]).)*)\}/));
+			input = input.replace(/\\left\(([0-9a-zA-Z\.\+\*\-\\\(\)\/]+)\\right\)\^\{(((?![\{\}]).)*)\}/g, "pow($1,$2)");
+		}
+
+		if (input.search(/\^/) >= 0) {
+			input = convert_others("nth power", input);
+		}
+		return input;
+	};
+
+	sin = function(input) {
+
+		return sinCosTanFramework("sin", input);
+	};
+
+	tan = function(input) {
+		return sinCosTanFramework("tan", input);
+	};
+
+	cos = function(input) {
+		return sinCosTanFramework("cos", input);
+	};
+
+	sinCosTanFramework = function(func, input) {
+		var pat1 = new RegExp("\\\\" + func + "\\\\left\\(([0-9a-zA-Z\\.\\+\\*\\-\\\\]+)\\\\right\\)");
+		//eg: /\\sin\\left\(([0-9a-zA-Z\.\+\*\-\\]+)\\right\)/
+
+		while (input.search(pat1) >= 0) {
+			console.log(input.match(pat1));
+			input = input.replace(pat1, func + "($1)");
+		}
+		var pat2 = new RegExp("\\\\" + func + "([0-9a-zA-Z]+)");
+		//eg:  /\\sin([0-9a-zA-Z]+)/:
+		console.log("input:" + input + input.match(pat2));
+		while (input.search(pat2) >= 0) {
+			console.log(input.match(pat2));
+			input = input.replace(pat2, func + "($1)");
+		}
+
+		var pat3 = new RegExp("\\\\" + func);
+		//eg:  /\\sin/
+		if (input.search(pat3) >= 0) {
+			input = convert_others(func, input);
+		}
+
+		return input;
+	};
+
+	convert_others = function(func, input) {
+		var temp_input, arr, closest_pos, func_pos, frac_pos, sqrt_pos, root_pos, pow_pos;
+		switch(func) {
+			case "fraction":
+				temp_input = input.match(/\\frac.*/)[0];
+				func_pos = temp_input.search(/\\frac/);
+				break;
+			case "square root":
+				temp_input = input.match(/\\sqrt\{.*/)[0];
+				func_pos = temp_input.search(/\\sqrt\{/);
+				break;
+			case "nth root":
+				temp_input = input.match(/\\sqrt\[.*/)[0];
+				func_pos = temp_input.search(/\\sqrt\[/);
+				break;
+			case "nth power":
+				temp_input = input.match(/\^.*/)[0];
+				func_pos = temp_input.search(/\^/);
+				break;
+			case "sin":
+				temp_input = input.match(/\\sin\{.*/)[0];
+				func_pos = temp_input.search(/\\sin\{/);
+				break;
+			case "tan":
+				temp_input = input.match(/\\tan\{.*/)[0];
+				func_pos = temp_input.search(/\\tan\{/);
+				break;
+			case "cos":
+				temp_input = input.match(/\\cos\{.*/)[0];
+				func_pos = temp_input.search(/\\cos\{/);
+				break;
+			default:
+				return;
+		}
+		frac_pos = temp_input.search(/\\frac/);
+		sqrt_pos = temp_input.search(/\\sqrt\{/);
+		root_pos = temp_input.search(/\\sqrt\[/);
+		pow_pos = temp_input.search(/\^/);
+		sin_pos = temp_input.search(/\\sin\{/);
+		tan_pos = temp_input.search(/\\tan\{/);
+		cos_pos = temp_input.search(/\\cos\{/);
+
+		arr = [frac_pos, root_pos, sqrt_pos, pow_pos, sin_pos, tan_pos, cos_pos];
+		arr.sort(function(a, b) {
+			return b - a;
+		});
+		//desc order
+
+		console.log(arr);
+
+		closest_pos = arr[arr.indexOf(0)-1];
+		
+		console.log("closest: "+closest_pos);
+
+		if (closest_pos <= 0 || closest_pos===undefined) {
+			console.log("ERROR: syntax error. func:"+func);
+			console.log(input);
+			throw ("syntax error");
+		}
+
+		switch(closest_pos) {
+			case frac_pos:
+				input = fraction(input);
+				break;
+			case sqrt_pos:
+				input = square_root(input);
+				break;
+			case root_pos:
+				input = nth_root(input);
+				break;
+			case pow_pos:
+				input = nth_power(input);
+				break;
+			case sin_pos:
+				input = sin(input);
+				break;
+			case tan_pos:
+				input = tan(input);
+				break;
+			case cos_pos:
+				input = cos(input);
+				break;
+			default:
+				console.log("syntax error");
+		}
+
+		switch(func) {
+			case "fraction":
+				input = fraction(input);
+				break;
+			case "square root":
+				input = square_root(input);
+				break;
+			case "nth root":
+				input = nth_root(input);
+				break;
+			case "nth power":
+				input = nth_power(input);
+				break;
+			case "sin":
+				input = sin(input);
+				break;
+			case "tan":
+				input = tan(input);
+				break;
+			case "cos":
+				input = cos(input);
+				break;
+		}
+
+		return input;
+	};
+	try{
+		return init();
+	}catch(e){
+		throw("syntax error");
+	}
+	
+};
+
